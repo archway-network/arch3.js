76ce45c6ce578c918de2beb2e56e8da460adda5e
Georges Chouchani <73434909+gachouchani1999@users.noreply.github.com>
2022-08-08 16:28:27 +0300

    Add signing Client  (#22)

--- packages/arch3-core/src/archwayclient.ts
+++ packages/arch3-core/src/archwayclient.ts
@@ -1,8 +1,15 @@
-import { CosmWasmClient } from '@cosmjs/cosmwasm-stargate';
+import { CosmWasmClient, SigningCosmWasmClient, SigningCosmWasmClientOptions } from '@cosmjs/cosmwasm-stargate';
+import { OfflineSigner } from "@cosmjs/proto-signing";
 import { Tendermint34Client } from '@cosmjs/tendermint-rpc';
 
 export class ArchwayClient extends CosmWasmClient {
   protected constructor(tmClient: Tendermint34Client | undefined) {
     super(tmClient);
+  }
+}
+
+export class SigningArchwayClient extends SigningCosmWasmClient {
+  protected constructor(tmClient: Tendermint34Client | undefined, signer: OfflineSigner, options: SigningCosmWasmClientOptions | undefined) {
+    super(tmClient, signer, options);
   }
 }
