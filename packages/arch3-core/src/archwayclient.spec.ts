baa6a7c1c33203763953b7eecf8213e3743fcaf5
Augusto Elesbão <aelesbao@users.noreply.github.com>
2022-08-31 19:47:48 +0200

    Add smoke tests for the clients (#28)
    * Upgrade @cosmjs dependencies
    
    * Test if clients are able to connect

--- packages/arch3-core/src/archwayclient.spec.ts
+++ packages/arch3-core/src/archwayclient.spec.ts
@@ -1,11 +1,52 @@
-describe('archwayClient', () => {
-  describe('metadata', () => {
-    describe('set', () => {
-      it.todo('can write a contract metadata');
-    });
+import { SigningCosmWasmClientOptions } from '@cosmjs/cosmwasm-stargate';
+import { DirectSecp256k1HdWallet } from '@cosmjs/proto-signing';
+import { GasPrice } from "@cosmjs/stargate";
 
-    describe('get', () => {
-      it.todo('can read a contract metadata');
+import { ArchwayClient, SigningArchwayClient } from './archwayclient';
+
+const wasmd = {
+  blockTime: 1_000, // ms
+  chainId: "archway-1",
+  endpoint: "https://rpc.constantine-1.archway.tech",
+  prefix: "arch",
+};
+
+const alice = {
+  mnemonic: "culture ten bar chase cross obey margin owner recycle trim valid logic forward mixed render have patrol dynamic tuition choose thing salute inside blossom",
+  pubkey0: {
+    type: "/cosmos.crypto.secp256k1.PubKey",
+    value: "AuoJH6FNYQVPNxRYSUGudoAIG3aXI+GvyazrmVFf2x/w",
+  },
+  address0: "archway1ecak50zhujddqd639xw4ejghnyrrc6jlwnlgwt",
+};
+
+const defaultGasPrice = GasPrice.fromString("0.02uarch");
+
+const defaultSigningClientOptions: SigningCosmWasmClientOptions = {
+  broadcastPollIntervalMs: 300,
+  broadcastTimeoutMs: 8_000,
+  gasPrice: defaultGasPrice,
+};
+
+describe('ArchwayClient', () => {
+  describe("connect", () => {
+    it("can be constructed", async () => {
+      const client = await ArchwayClient.connect(wasmd.endpoint);
+      expect(client).toBeTruthy();
+    });
+  });
+});
+
+describe('SigningArchwayClient', () => {
+  describe("connectWithSigner", () => {
+    it("can be constructed", async () => {
+      const wallet = await DirectSecp256k1HdWallet.fromMnemonic(alice.mnemonic, { prefix: wasmd.prefix });
+      const client = await SigningArchwayClient.connectWithSigner(wasmd.endpoint, wallet, {
+        ...defaultSigningClientOptions,
+        prefix: wasmd.prefix,
+      });
+      expect(client).toBeTruthy();
+      client.disconnect();
     });
   });
 });
