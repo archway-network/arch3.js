f1926665726e4ab1722a6289d3ae32c8fe8db96c
Augusto Elesbão <aelesbao@users.noreply.github.com>
2022-09-20 18:57:45 +0200

    fix: override clients' connect method (#31)

--- packages/arch3-core/src/archwayclient.ts
+++ packages/arch3-core/src/archwayclient.ts
@@ -1,4 +1,4 @@
-import { CosmWasmClient, SigningCosmWasmClient, SigningCosmWasmClientOptions } from '@cosmjs/cosmwasm-stargate';
+import { CosmWasmClient, HttpEndpoint, SigningCosmWasmClient, SigningCosmWasmClientOptions } from '@cosmjs/cosmwasm-stargate';
 import { OfflineSigner } from "@cosmjs/proto-signing";
 import { Tendermint34Client } from '@cosmjs/tendermint-rpc';
 
@@ -6,10 +6,31 @@ export class ArchwayClient extends CosmWasmClient {
   protected constructor(tmClient: Tendermint34Client | undefined) {
     super(tmClient);
   }
+
+  public static override async connect(endpoint: string | HttpEndpoint): Promise<ArchwayClient> {
+    const tmClient = await Tendermint34Client.connect(endpoint);
+    return new ArchwayClient(tmClient);
+  }
 }
 
 export class SigningArchwayClient extends SigningCosmWasmClient {
   protected constructor(tmClient: Tendermint34Client | undefined, signer: OfflineSigner, options: SigningCosmWasmClientOptions | undefined) {
     super(tmClient, signer, options);
+  }
+
+  public static override async connectWithSigner(
+    endpoint: string | HttpEndpoint,
+    signer: OfflineSigner,
+    options: SigningCosmWasmClientOptions = {},
+  ): Promise<SigningArchwayClient> {
+    const tmClient = await Tendermint34Client.connect(endpoint);
+    return new SigningArchwayClient(tmClient, signer, options);
+  }
+
+  public static override async offline(
+    signer: OfflineSigner,
+    options: SigningCosmWasmClientOptions = {},
+  ): Promise<SigningArchwayClient> {
+    return new SigningArchwayClient(undefined, signer, options);
   }
 }
