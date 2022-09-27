cc866e1a51c1f0f902d72a487bd41c395dd79db1
Augusto Elesbão <aelesbao@gmail.com>
2022-09-27 10:14:48 -0500

    chore: change auto-commit message

--- scripts/import-proto.sh
+++ scripts/import-proto.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env sh
 
-set -euo pipefail
+set -eu
 
 TMPDIR="$(mktemp -d)"
 git clone https://github.com/archway-network/archway --depth=1 --single-branch "$TMPDIR"
 
 REPO_ROOT="$(git rev-parse --show-toplevel)"
-[[ -d "$REPO_ROOT/proto" ]] && rm -rf "$REPO_ROOT/proto"
+[ -d "$REPO_ROOT/proto" ] && rm -rf "$REPO_ROOT/proto"
 cp -r "$TMPDIR/proto" "$REPO_ROOT"
 git add "$REPO_ROOT/proto"
-git commit -m "chore: updated proto files"
+git commit -m "build: update proto files"
