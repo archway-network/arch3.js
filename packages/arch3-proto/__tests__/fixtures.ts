58ea7457d77d5651bdd7b98269d57ab4715f153d
David Choi <david.choi@philabs.xyz>
2023-03-15 19:42:00 -0400

    fix: remove unneeded tests of codegen

--- packages/arch3-proto/__tests__/fixtures.ts
+++
@@ -1,5 +0,0 @@
-export const RPC_ENDPOINT = 'https://rpc.constantine-1.archway.tech';
-export const CONSTANTINE_1_DEVX_TEST_ACCOUNT =
-  'archway1rev2n7edzn6l84k37dhnhs0m9wqlveezvwjj38';
-export const CONSTANTINE_AIRDROP_CONTRACT =
-  'archway16fpjs4u9pq7px3h3zaawegtfp63dnerm83rjywjn7j9t8g0vtu9qhvgvsr';
