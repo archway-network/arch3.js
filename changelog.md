8027d87d32f09970a5bfe8d8969c347fc5423ffb
David Choi <david.choi@philabs.xyz>
2023-03-15 20:37:10 -0400

    fix: getProtoImports.ts for linting and updated changelog

--- changelog.md
+++ changelog.md
@@ -5,6 +5,18 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](http://keepachangelog.com/)
 and this project adheres to [Semantic Versioning](http://semver.org/).
  
+## [0.1.2] - 2023-03-15
+ 
+This change adds a script to gather the list of protobuf imports and then adds them into the readme.
+ 
+### Added
+- [arch3.js-0.1.2](https://github.com/archway-network/arch3.js)
+  Document protobuf / protocol version dependancies within the readme.md.
+ 
+### Changed
+ 
+### Fixed
+
 ## [0.1.1] - 2023-03-15
  
 This change is a workspace addition of protobufs codegen capabilities. The new package arch3-proto is the core change.
--- packages/arch3-proto/src/getProtoImports.ts
+++ packages/arch3-proto/src/getProtoImports.ts
@@ -1,4 +1,4 @@
-import { createReadStream, writeFileSync, existsSync } from 'fs';
+import { createReadStream, writeFileSync } from 'fs';
 import { createInterface } from 'readline';
 import { resolve } from 'path';
 import { readdir } from 'fs/promises';
@@ -6,64 +6,59 @@ import { readdir } from 'fs/promises';
 main();
 
 async function main() {
-    // get all proto files on root of proto folder
-    const allProtoFiles = await getFilesRecursive("../../../proto");
-    // read file and grab only lines starting with "import "
-    const imports = [];
-    for (let i = 0; i < allProtoFiles.length; i++) {
-        const stream = createReadStream(allProtoFiles[i].filePath);
-        const lines = await createInterface({
-            input: stream,
-            crlfDelay: Infinity
-        });
-        // insert only new entries
-        for await (const line of lines) {
-            if (line.startsWith("import ")) {
-                console.log("line", line);
-                const readLine = line.replace("import ", "");
-                const cleanedLine = clean(readLine);
-                if (!imports.includes(cleanedLine)) {
-                    imports.push(cleanedLine);
-                }
-            }
-        }
-    }
-    const sortedImports = imports.sort((a, b) => {
-        if(a > b) return 1;
-        if (a < b) return -1;
-        return 0;
+  const allProtoFiles = await getFilesRecursive('../../../proto');
+  const imports = [];
+  for (let i = 0; i < allProtoFiles.length; i++) {
+    const stream = createReadStream(allProtoFiles[i].filePath);
+    const lines = await createInterface({
+      input: stream,
+      crlfDelay: Infinity
     });
-    console.log("imports", sortedImports);
-    let output = "";
-    for (let l = 0; l < sortedImports.length; l++) {
-        output += '- ' + sortedImports[l] + '\n';
+
+    for await (const line of lines) {
+      if (line.startsWith('import ')) {
+        const readLine = line.replace('import ', '');
+        const cleanedLine = clean(readLine);
+        if (!imports.includes(cleanedLine)) {
+          imports.push(cleanedLine);
+        }
+      }
     }
-    // create new empty destination file to contain all proto import paths
-    writeFileSync("protoImports.txt", output);
+  }
+  const sortedImports = imports.sort((a, b) => {
+    if (a > b) return 1;
+    if (a < b) return -1;
+    return 0;
+  });
+
+  let output = '';
+  for (let l = 0; l < sortedImports.length; l++) {
+    output += '- ' + sortedImports[l] + '\n';
+  }
+
+  writeFileSync('protoImports.txt', output);
 }
 
 function clean(item: string) {
-    return item
-        .replaceAll('"', '')
-        .replaceAll(';', '');
+  return item.replaceAll('"', '').replaceAll(';', '');
 }
 
-async function getFilesRecursive(dirPath: string): Promise<{ filePath: string, fileName: string }[]> {
-    const fsObjects = (await readdir(dirPath, { withFileTypes: true }));
-    let filesOnly: { filePath: string, fileName: string }[] = [];
+async function getFilesRecursive(
+  dirPath: string
+): Promise<{ filePath: string; fileName: string }[]> {
+  const fsObjects = await readdir(dirPath, { withFileTypes: true });
+  let filesOnly: { filePath: string; fileName: string }[] = [];
 
-    // use of for let is not an error do not use forEach for larger arrays
-    for (let i = 0; i < fsObjects.length; i++) {
-        const fsObj = fsObjects[i];
-        let filePathWithFs = resolve(dirPath, fsObj.name);
+  for (let i = 0; i < fsObjects.length; i++) {
+    const fsObj = fsObjects[i];
+    const filePathWithFs = resolve(dirPath, fsObj.name);
 
-        if (fsObj.isDirectory()) {
-            // todo: need to optimize, spread is very slow
-            filesOnly = [...filesOnly, ...(await getFilesRecursive(filePathWithFs))];
-        } else if (fsObj.name.includes(".proto")) {
-            filesOnly.push({ filePath: filePathWithFs, fileName: fsObj.name });
-        }
-    };
+    if (fsObj.isDirectory()) {
+      filesOnly = [...filesOnly, ...(await getFilesRecursive(filePathWithFs))];
+    } else if (fsObj.name.includes('.proto')) {
+      filesOnly.push({ filePath: filePathWithFs, fileName: fsObj.name });
+    }
+  }
 
-    return filesOnly;
+  return filesOnly;
 }
