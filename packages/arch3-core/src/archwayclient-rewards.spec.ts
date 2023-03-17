4e25bdc3dc873ca3a6c0d289409f4ecab0ae7719
David Choi <david.choi@philabs.xyz>
2023-03-17 17:12:00 -0400

    chore: add method and test

--- packages/arch3-core/src/archwayclient-rewards.spec.ts
+++ packages/arch3-core/src/archwayclient-rewards.spec.ts
@@ -1,7 +1,10 @@
+import { Long } from '@archwayhq/arch3-proto/src/codegen/helpers';
+
 import { ArchwayClient } from './archwayclient';
 
 const rpcUrl = 'https://rpc.constantine-1.archway.tech';
 const airdropContract = 'archway16fpjs4u9pq7px3h3zaawegtfp63dnerm83rjywjn7j9t8g0vtu9qhvgvsr';
+const denom = 'uconst';
 
 describe('Archway Rewards', () => {
   it('check block rewards is coming back', async () => {
@@ -21,5 +24,20 @@ describe('Archway Rewards', () => {
 
     // note: some contracts have no metadata defined
     expect(response === undefined || response.metadata).toBe(true);
+  });
+
+  it('check estimate fees is coming back', async () => {
+    await ArchwayClient.connect(rpcUrl);
+
+    const response = await ArchwayClient.getEstimateFees({
+      gasLimit: new Long(100000),
+      contractAddress: airdropContract
+    });
+
+    // note: some contracts have no metadata defined
+    expect(response.estimatedFee[0].denom).toBe(denom);
+    expect(typeof Number(response.estimatedFee[0].amount)).toBe('number');
+    expect(response.gasUnitPrice?.denom).toBe(denom);
+    expect(typeof Number(response.gasUnitPrice?.amount)).toBe('number');
   });
 });
