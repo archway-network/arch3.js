5c45f49f9041027655e21499cd03a2bade3620f9
David Choi <david.choi@philabs.xyz>
2023-03-20 17:49:37 -0400

    fix: try prevent ts-jest from process archwayclient.ts

--- jest.config.ts
+++ jest.config.ts
@@ -109,7 +109,7 @@ export default {
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
   transformIgnorePatterns: [
-    "<rootDir>/../(?!.arch3-proto)"
+    "<rootDir>/src/(?!archwayclient.ts/)"
   ],
 
   // An array of regexp pattern strings that are matched against all modules before the module loader will automatically return a mock for them
