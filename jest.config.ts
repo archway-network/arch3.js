bf2ad10bce16d4cadbdf2a009ab21a6a582aef32
David Choi <david.choi@philabs.xyz>
2023-03-20 19:11:38 -0400

    chore: try reset moduleNameMapper

--- jest.config.ts
+++ jest.config.ts
@@ -83,11 +83,11 @@ export default {
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
   moduleNameMapper: {
-    "^@archwayhq/arch3-proto/(.*)": "<rootDir>/../arch3-proto/$1"
+    "^@archwayhq/arch3-proto/(.*)$": "<rootDir>/../arch3-proto/$1"
   },
 
   // A preset that is used as a base for Jest's configuration
-  preset: 'ts-jest/legacy',
+  preset: 'ts-jest',
 
   // The test environment that will be used for testing
   testEnvironment: 'node',
