80bc8bb9f4aedd681372fb345b726ba82a5e039c
David Choi <david.choi@philabs.xyz>
2023-03-31 14:40:33 -0400

    fix: try using address only and fix source dir issue on deploy.sh

--- scripts/deploy-contract.sh
+++ scripts/deploy-contract.sh
@@ -2,7 +2,7 @@
 
 # set -euo pipefail
 
-deployer_name="${1:-deployer}"
+deployer_addr="${1:-deployer}"
 
 cd scripts/contract/arch3test/scripts
-sh deploy.sh "${deployer_name}"
+sh deploy.sh "${deployer_addr}"
