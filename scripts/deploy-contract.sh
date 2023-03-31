f3bbb21b96a4d2c74539973a412df370681cabaf
David Choi <david.choi@philabs.xyz>
2023-03-31 13:41:48 -0400

    fix: refactor for new contract to test on github

--- scripts/deploy-contract.sh
+++ scripts/deploy-contract.sh
@@ -1,9 +1,8 @@
-#!/bin/bash
+#!/bin/sh
 
-set -euo pipefail
+# set -euo pipefail
 
 deployer_name="${1:-deployer}"
 
-#SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 cd scripts/contract/arch3test/scripts
 sh deploy.sh "${deployer_name}"
