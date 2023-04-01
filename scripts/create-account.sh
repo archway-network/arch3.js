3206552c08806f666b17e2f8fe81de35afd0b8ad
David Choi <david.choi@philabs.xyz>
2023-04-01 00:03:01 -0400

    fix: refactor test.yml for expect script

--- scripts/create-account.sh
+++ scripts/create-account.sh
@@ -4,16 +4,9 @@ set timeout -1
 
 puts "Start create testing account"
 
-DEVX_MNEMONIC="${DEVX_MNEMONIC:-}"
-
-archwayd config chain-id "local-1"
-
 spawn archwayd keys add --recover ""
 
 expect "Enter your bip39 mnemonic"
-send -- "${DEVX_MNEMONIC}\r"
+send -- "$env(DEVX_MNEMONIC)\r"
 
 expect eof
-
-archwayd config
-archwayd keys list
