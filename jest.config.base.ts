dffdd7ef26d9a57efbcb08491370eae6cddc8cda
Augusto Elesbão <aelesbao@users.noreply.github.com>
2023-04-27 01:58:06 +0200

    build(deps): use dotenv for testing

--- jest.config.base.ts
+++ jest.config.base.ts
@@ -40,6 +40,11 @@ export const baseConfig: JestConfigWithTsJest = {
   // A preset that is used as a base for Jest's configuration
   preset: 'ts-jest',
 
+  // A list of paths to modules that run some code to configure or set up the testing environment
+  setupFiles: [
+    'dotenv/config'
+  ],
+
   // The test environment that will be used for testing
   testEnvironment: 'node',
 
