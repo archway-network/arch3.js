bd2a95b350a91825e49bf6cd484396eb2931250f
Augusto Elesbão <aelesbao@users.noreply.github.com>
2023-05-08 17:42:03 +0200

    chore: import Archway files from a specific branch

--- scripts/import-contracts.sh
+++ scripts/import-contracts.sh
@@ -4,5 +4,7 @@ set -euo pipefail
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 
-curl -sSfL 'https://github.com/archway-network/archway/raw/main/contracts/go/voter/code.wasm' \
+ARCHWAY_BRANCH="${ARCHWAY_BRANCH:-main}"
+
+curl -sSfL "https://github.com/archway-network/archway/raw/${ARCHWAY_BRANCH}/contracts/go/voter/code.wasm" \
   -o "${SCRIPT_DIR}/contracts/voter.wasm"
--- scripts/import-proto.sh
+++ scripts/import-proto.sh
@@ -1,13 +1,29 @@
-#!/usr/bin/env sh
+#!/bin/bash
 
-set -eu
+set -euo pipefail
+
+ARCHWAY_BRANCH="${ARCHWAY_BRANCH:-main}"
 
 TMPDIR="$(mktemp -d)"
-git clone https://github.com/archway-network/archway --depth=1 --single-branch "$TMPDIR"
+git clone https://github.com/archway-network/archway --depth=1 --single-branch -b "${ARCHWAY_BRANCH}" "$TMPDIR"
 
 PROTO_ROOT="$(git rev-parse --show-toplevel)/packages/arch3-proto"
 [ -d "$PROTO_ROOT/proto" ] && rm -rf "$PROTO_ROOT/proto"
+
 cp -r "$TMPDIR/proto" "$PROTO_ROOT"
 cp -r "$TMPDIR/third_party/proto" "$PROTO_ROOT"
-git add "$PROTO_ROOT/proto"
-git commit -m "build: update proto files"
+
+cat <<EOF
+
+$(tput setaf 2)✔$(tput sgr0) Proto files imported from archway-network/archway@${ARCHWAY_BRANCH}.
+
+$(tput setaf 6)To re-generate the client files, run the codegen script:
+$(tput setaf 7)
+yarn workspace @archwayhq/arch3-proto codegen
+$(tput sgr0)
+$(tput setaf 6)After reviewing and testing, commit the changes using:
+$(tput setaf 7)
+git add packages/arch3-proto/{generated,proto}
+git commit -m "build(arch3-proto): update and generate files"
+$(tput sgr0)
+EOF
