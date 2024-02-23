83bcbd53944b4a4281742a373a0c527b2cba482d
eliasmpw <eliasmpw@hotmail.com>
2024-02-23 20:03:15 +0100

    fix: lint of single quotes

--- packages/arch3-core/src/utils.ts
+++ packages/arch3-core/src/utils.ts
@@ -1,4 +1,4 @@
-import { Tendermint37Client, RpcClient, Comet38Client, Tendermint34Client, CometClient } from "@cosmjs/tendermint-rpc";
+import { Tendermint37Client, RpcClient, Comet38Client, Tendermint34Client, CometClient } from '@cosmjs/tendermint-rpc';
 
 /**
  * Auto-detects Tendermint/CometBFT 0.34/0.37/0.38 version for connecting to an RPC Client.
@@ -10,13 +10,13 @@ export async function connectToRpcClient(rpcClient: RpcClient): Promise<CometCli
   const tendermint37Client = await Tendermint37Client.create(rpcClient);
   const version = (await tendermint37Client.status()).nodeInfo.version;
 
-  if (version.startsWith("0.37.")) {
+  if (version.startsWith('0.37.')) {
     return tendermint37Client;
   }
 
   tendermint37Client.disconnect();
 
-  if (version.startsWith("0.38.")) {
+  if (version.startsWith('0.38.')) {
     return await Comet38Client.create(rpcClient);
   }
 
