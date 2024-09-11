5835b14740d0b3f96646467238f73373aa5e40a0
eliasmpw <eliasmpw@hotmail.com>
2024-09-11 20:03:33 +0200

    fix(core): workaround for batching issue in V9

--- packages/arch3-core/src/utils.ts
+++ packages/arch3-core/src/utils.ts
@@ -10,15 +10,17 @@ export async function connectToRpcClient(rpcClient: RpcClient): Promise<CometCli
   const tendermint37Client = await Tendermint37Client.create(rpcClient);
   const version = (await tendermint37Client.status()).nodeInfo.version;
 
-  if (version.startsWith('0.37.')) {
+  // HACK: Force Tendermint37Client on 0.38.x versions, remove when client batching issue is fixed
+  if (version.startsWith('0.37.') || version.startsWith('0.38.')) {
     return tendermint37Client;
   }
 
   tendermint37Client.disconnect();
 
-  if (version.startsWith('0.38.')) {
-    return await Comet38Client.create(rpcClient);
-  }
+  // if (version.startsWith('0.38.')) {
+  //   console.log('VERRRRR2222',version)
+  //   return await Comet38Client.create(rpcClient);
+  // }
 
   return await Tendermint34Client.create(rpcClient);
 }
